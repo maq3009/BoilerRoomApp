816f787a714d1a6ab25bbaa01862e68da5c22d92
Misael <maq3009@hotmail.com>
2023-11-26 01:00:05 -0400

    fixed firebaseConfig and uploading to play store

--- BoileRoomApp/firebaseConfig.ts
+++ BoileRoomApp/firebaseConfig.ts
@@ -1,9 +1,9 @@
 import { initializeApp, FirebaseApp } from 'firebase/app';
-import { initializeAuth, User, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth, User} from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-
+import * as firebaseAuth from 'firebase/auth';
 
 
 //My Database Configuration
@@ -21,13 +21,13 @@ const firebaseConfig = {
 
 
 // initialize Firebase
-
+const reactNativePersistence = (firebaseAuth as any).getReactNativePersistence;
 const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
 const firestore = getFirestore(firebaseApp);
 
 
 const auth = initializeAuth(firebaseApp, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+    persistence: reactNativePersistence(ReactNativeAsyncStorage)
   });
 
 
